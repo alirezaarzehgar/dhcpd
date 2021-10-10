74e2d7614c95fce01ec9c78487964437afe0cd75
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-10 11:12:39 +0330

    init

--- src/dhcpd.c
+++ src/dhcpd.c
@@ -13,9 +13,60 @@
 
 #include "network/listener.h"
 
+dhcpNetworkPktInfo_t
+getReplyDependencies (pktDhcpPacket_t
+                              *discovery)
+{
+  dhcpNetworkPktInfo_t info =
+  {
+    .fields = {
+      {.func = (pktGenCallbackFunc_t)pktGenFieldYourIpAddress, .param = TEST_FAKE_DATA_DHCP_NETWORK_YOUR_IP_ADDRESS},
+      PKT_GEN_CALLBACK_NULL,
+    },
+
+    .options =
+    {
+      {.func = (pktGenCallbackFunc_t)pktGenOptDhcpServerIdentifier, .param = TEST_FAKE_DATA_DHCP_NETWORK_SERVER_IDENTIFIER},
+      {.func = (pktGenCallbackFunc_t)pktGenOptIpAddrLeaseTime, .param = (void *)TEST_FAKE_DATA_DHCP_NETWORK_IP_ADDRESS_LEASE_TIME},
+      {.func = (pktGenCallbackFunc_t)pktGenOptSubnetMask, .param = TEST_FAKE_DATA_DHCP_NETWORK_SUBNET_MASK},
+      {.func = (pktGenCallbackFunc_t)pktGenOptRouter, .param = TEST_FAKE_DATA_DHCP_NETWORK_ROUTER},
+      {.func = (pktGenCallbackFunc_t)pktGenOptDomainName, .param = TEST_FAKE_DATA_DHCP_NETWORK_DOMAIN_NAME},
+      PKT_GEN_CALLBACK_NULL,
+    }
+  };
+
+  (void *)discovery;      /* Quit unused warning */
+
+  return info;
+}
+
+char *
+ackHandler (pktDhcpPacket_t *pkt)
+{
+  printf ("lease!");
+  return NULL;
+}
+
 int
 main (int argc, char const *argv[])
 {
-  printf ("hello dhcpd\n");
+  /* declaration */
+  int retval;
+
+  char address[PKT_ADDR_MAX_LEN];
+
+  int port;
+
+  /* definition */
+  strcpy (address, "192.168.133.30");
+
+  port = 67;
+
+  retval = dhcpNetworkListener (address, port, getReplyDependencies,
+                                getReplyDependencies, ackHandler);
+
+  if (retval == EXIT_FAILURE)
+    perror ("dhcpNetworkListener");
+
   return 0;
 }
