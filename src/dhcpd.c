88541191c09394770fea002ca4f39a9be4fe38f8
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-10 10:19:32 +0330

    init

---
+++ src/dhcpd.c
@@ -0,0 +1,19 @@
+/**
+ * @file dhcpd.c
+ * @author alirezaarzehgar (alirezaarzehgar82@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-10-10
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "dhcpd/dhcpd.h"
+
+int
+main (int argc, char const *argv[])
+{
+  printf ("hello dhcpd\n");
+  return 0;
+}
